57df5372e3f67fcaff8d1576665aef735616572d
GaryCho <honor1998321@gmail.com>
2024-10-03 23:23:20 -0500

    show

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Button from './components/Button/Button';
+import Dropdown from './components/Dropdown/Dropdown';
+import Table from './components/Table/Table';
+import Card from './components/Card/Card';
+import RadioButton from './components/RadioButton/RadioButton';
+import Text from './components/Text/Text';
+import HeroImage from './components/HeroImage/HeroImage';
+import Img from './components/Img/Img';
+import Label from './components/Label/Label';
+import SampleImg from './assets/Sample.jpg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h2>Web Component Library </h2>
+        <Button label="Click Me" />
+        <br />
+        <Dropdown options={[{ label: 'Option 1', value: '1' }]} />
+        <br />
+        <Table
+          headers={['ID', 'Name']}
+          data={[
+            {id: 1,name: 'Row 1'},
+            {id: 2,name: 'Row 2'},
+          ]}
+        />
+        <br />
+        <Card title="Card Title" content="This is a sample card content." />
+        <RadioButton label="Radio Option" value="1" name="radio-group" />
+        <Text content="Sample Text" />
+        <Label text="Sample Label" />
+        <HeroImage imageUrl={SampleImg} />
+        <br />
+        <Img src={SampleImg} alt="Sample Image" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
